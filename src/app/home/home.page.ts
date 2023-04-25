4679b96f6c6e6b9be252dfc4cb3b4283e86c7fa5
Mohit Paryani <>
2023-04-26 02:25:33 +0530

    Added Button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  text = '';
+
+  constructor() {
+    var path = "file:///storage/emulated/0";
+    var filename = "myfile.txt";
+
+    /*
+    https://devdactic.com/capacitor-file-explorer-ionic
+    https://stackoverflow.com/questions/47069764/ionic-delete-file-using-native-url
+    https://ourcodeworld.com/articles/read/28/how-to-delete-file-with-cordova
+    https://github.com/apache/cordova-plugin-file
+    https://forum.ionicframework.com/t/delete-image-from-gallery/26433
+    */
+
+
+
+    /* window.resolveLocalFileSystemURL(path, function(dir) {
+        dir.getFile(filename, {create:false}, function(fileEntry) {
+                  fileEntry.remove(function(){
+                    this.text = 'removed';
+                    // The file has been removed succesfully
+                  },function(error){
+                    this.text = 'error';
+                    // Error deleting the file
+                  },function(){
+                    this.text = 'file doesn\'t exist';
+                    // The file doesn't exist
+                  });
+        });
+    }); */
+  }
 
 }
